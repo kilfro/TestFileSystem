34cd47ba7e86ed7b70e3185144f310e15439706a
Kirill Frolov <kilfro@hotmail.com>
2017-04-24 13:40:03 +0300

    fix test import & pom

--- src/main/java/exception/NotFoundException.java
+++ src/main/java/exception/NotFoundException.java
@@ -7,7 +7,4 @@ public class NotFoundException extends Exception {
     public NotFoundException(String message) {
         super(message);
     }
-
-    public NotFoundException() {
-    }
 }
--- src/test/java/system/InMemorySystemTest.java
+++ src/test/java/system/InMemorySystemTest.java
@@ -8,8 +8,6 @@ import model.TransferModel;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
